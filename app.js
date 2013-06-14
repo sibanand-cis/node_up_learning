91978abd591c151f3a7dece418fccf3dbc46c8f7
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-06-14 13:06:44 +0530

    ex 2.2 add views related code to app

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var express = require('express');
+// application using express engine and jade for template 
 
+var express = require('express');
 var app = express();
+
+// add views to app.js
+app.set('view engine', 'jade');
+app.set('view options', { layout: true});
+app.set('views', __dirname + '/views'); //This command tells Express that all of its views should be drawn from folder named "views" which is subordinate to the directoryfrom where the code file is being run(__dirname)
 
 app.get('/stooges/:name?', function(req,res,next){
 	var name = req.params.name.toUpperCase();
@@ -10,7 +16,7 @@ app.get('/stooges/:name?', function(req,res,next){
 		case 'larry':
 		case 'curly':
 		case 'moe':
-			res.send(name + ' is my favorite stooge');
+			res.render('stooges', {stooge: name});
 			break;
 
 		default:
@@ -19,11 +25,11 @@ app.get('/stooges/:name?', function(req,res,next){
 });
 
 app.get('/stooges/*?',function(req,res){
-	res.send('no stooges listed');
+	res.render('stooges', {stooge: null});
 });
 
 app.get('/?',function(req,res){
-	res.send('hello world')
+	res.render('index')
 });
 
 var port = 3000;
